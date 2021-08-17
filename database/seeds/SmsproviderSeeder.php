51cb8a6baf136882f92992b408fe7cdf7307512a
Arpita singh <badhaiiho arpita>
2021-08-17 06:51:40 +0530

    change seeder file

--- database/seeds/SmsproviderSeeder.php
+++ database/seeds/SmsproviderSeeder.php
@@ -17,22 +17,19 @@ class SmsproviderSeeder extends Seeder
 
             // [
             //     'providername' => 'TextLocal',
-            //     'username' => 'arpitasingh820@gmail.com',
-            //     'sendurl' => 'http://api.textlocal.in/send/',
+            //     'sendurl' => 'https://api.textlocal.in/send/?username=arpitasingh820@gmail.com&hash=b64d3af4609a5eb30bbdaf5ede68855e6dd7849e02f0cffe36785eaca67beabd&test=0',
             //     'provider' => 'TXTLCL',
             //     'apikey' => 'b64d3af4609a5eb30bbdaf5ede68855e6dd7849e02f0cffe36785eaca67beabd',
-            //     'route' => '0'
+          
 
             // ],
             [
 
                 'providername' => 'Fast2sms',
-                'username' => '',
-                'sendurl' => 'https://www.fast2sms.com/dev/bulkV2',
+                'sendurl' => 'https://www.fast2sms.com/dev/bulkV2?route=v3',
                 'provider' => 'TXTIND',
                 'apikey' => 'xaL2gNQE1uVXt0cenlBzYZfmkyjO7iC94v6H3PwMoKJbTsAWSIXJ70uUAdMlj2Bq4e6mzr9FOZcsGR1n',
-                'route' => 'v3'
-
+  
             ]
 
 
